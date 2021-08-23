bd8f1fcc16565bffa22b1c5840884ae717bc0987
Manasa S M <manasarow2001@gmail.com>
2021-08-24 00:12:08 +0530

    Added signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import Home from './Pages/Home/home';
 import LoginForm from './Pages/Login/Login';
+import SignupForm from './Pages/Signup/Signup';
 import Canteens from './Pages/canteens'
 import MyOrders from './Pages/myorders'
 import Cart from './Pages/cart';
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/login' exact component={LoginForm} />
+        <Route path='/signup' exact component={SignupForm} />
         <Route path='/canteens' component={Canteens} />
         <Route path='/myorders' component={MyOrders} />
         <Route path='/cart' component={Cart} />
